110cf3d59a1388a1117eb60b3d5e99c8a55c91ff
Brenden Forbis <brenden.forbis@outlook.com>
2020-10-03 14:45:58 -0400

    Updates

---
+++ precache-manifest.59d0450ff11e3bf2a8e61d6ea37080e5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36677f093cd3ba51be0eb5d30716a661",
+    "url": "/new-repo/index.html"
+  },
+  {
+    "revision": "7fa4a86c39fcf27b742a",
+    "url": "/new-repo/static/css/main.8286826f.chunk.css"
+  },
+  {
+    "revision": "dfff7ac762902de77de6",
+    "url": "/new-repo/static/js/2.64fa2420.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-repo/static/js/2.64fa2420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa4a86c39fcf27b742a",
+    "url": "/new-repo/static/js/main.97a4aded.chunk.js"
+  },
+  {
+    "revision": "18e74ac28f90f30dfe5d",
+    "url": "/new-repo/static/js/runtime-main.9e39ce94.js"
+  },
+  {
+    "revision": "bb41240923c08db9995ab7a3dbf16d32",
+    "url": "/new-repo/static/media/biopic.bb412409.jpg"
+  },
+  {
+    "revision": "fc0d42758e969f46153695ce0cfb0f06",
+    "url": "/new-repo/static/media/coding-quiz.fc0d4275.JPG"
+  },
+  {
+    "revision": "040e70606790b5c02f9e549c70ccd12f",
+    "url": "/new-repo/static/media/cyclepaths.040e7060.JPG"
+  },
+  {
+    "revision": "ec64814fd25e683d2c0ddc6899493fbc",
+    "url": "/new-repo/static/media/generator.ec64814f.JPG"
+  },
+  {
+    "revision": "d31779db99d5dc93b1c4ec0ad6f00028",
+    "url": "/new-repo/static/media/github-icon.d31779db.png"
+  },
+  {
+    "revision": "15fd5adb3c750e71a0e6ba69caaabe5f",
+    "url": "/new-repo/static/media/linkedin-icon.15fd5adb.png"
+  },
+  {
+    "revision": "84c12bebc9a93a10dac749c10b4d84a7",
+    "url": "/new-repo/static/media/planner.84c12beb.JPG"
+  },
+  {
+    "revision": "1d5ad88b3070f32d2967b807b4064a26",
+    "url": "/new-repo/static/media/weather.1d5ad88b.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.61bb70f56efdcd8c65e77fd842a4bb8b.js"
+  "/new-repo/precache-manifest.59d0450ff11e3bf2a8e61d6ea37080e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-repo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
