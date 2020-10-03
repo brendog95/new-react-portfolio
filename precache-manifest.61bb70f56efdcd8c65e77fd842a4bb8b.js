b528f1295175bf1fdced1de904a826576be203f1
Brenden Forbis <brenden.forbis@outlook.com>
2020-10-03 14:43:17 -0400

    Updates

---
+++ precache-manifest.61bb70f56efdcd8c65e77fd842a4bb8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f4fba93a6a9e77c55929647de6a982",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7fa4a86c39fcf27b742a",
+    "url": "/portfolio/static/css/main.8286826f.chunk.css"
+  },
+  {
+    "revision": "dfff7ac762902de77de6",
+    "url": "/portfolio/static/js/2.64fa2420.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.64fa2420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa4a86c39fcf27b742a",
+    "url": "/portfolio/static/js/main.97a4aded.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bb41240923c08db9995ab7a3dbf16d32",
+    "url": "/portfolio/static/media/biopic.bb412409.jpg"
+  },
+  {
+    "revision": "fc0d42758e969f46153695ce0cfb0f06",
+    "url": "/portfolio/static/media/coding-quiz.fc0d4275.JPG"
+  },
+  {
+    "revision": "040e70606790b5c02f9e549c70ccd12f",
+    "url": "/portfolio/static/media/cyclepaths.040e7060.JPG"
+  },
+  {
+    "revision": "ec64814fd25e683d2c0ddc6899493fbc",
+    "url": "/portfolio/static/media/generator.ec64814f.JPG"
+  },
+  {
+    "revision": "d31779db99d5dc93b1c4ec0ad6f00028",
+    "url": "/portfolio/static/media/github-icon.d31779db.png"
+  },
+  {
+    "revision": "15fd5adb3c750e71a0e6ba69caaabe5f",
+    "url": "/portfolio/static/media/linkedin-icon.15fd5adb.png"
+  },
+  {
+    "revision": "84c12bebc9a93a10dac749c10b4d84a7",
+    "url": "/portfolio/static/media/planner.84c12beb.JPG"
+  },
+  {
+    "revision": "1d5ad88b3070f32d2967b807b4064a26",
+    "url": "/portfolio/static/media/weather.1d5ad88b.JPG"
+  }
+]);
